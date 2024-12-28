d3c2976f101f1b12481e9a1f1db9168b323ad84d
George Michael Brower <george.brower@gmail.com>
2024-12-28 17:23:46 -0500

    update vue test case

--- examples/vue/main.vue
+++ examples/vue/main.vue
@@ -1,16 +1,12 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h1>Hello from minimal Vue SFC!</h1>
+  <h1>Hello from minimal Vue SFC! {{ test }}</h1>
 </template>
 
-<script lang="ts">
-export default {
-	name: 'App',
-	data() {
-		return {
-			msg: 'Hello from minimal Vue SFC!'
-		};
-	}
-};
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const test = ref( 'test' );
 </script>
 
 <style scoped>
