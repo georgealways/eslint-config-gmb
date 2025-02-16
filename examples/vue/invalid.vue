76328942d432ac08c457747f4a6feeb2aea28fde
George Michael Brower <george.brower@gmail.com>
2025-02-16 17:25:40 -0500

    spaces to tabs in css for vue sfc's

--- examples/vue/invalid.vue
+++ examples/vue/invalid.vue
@@ -1,16 +1,15 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue';
-
-import HelloWorld from '@/components/HelloWorld.vue';
-
 import config, {
-	js,
 	imports,
+	js,
 } from 'eslint-config-gmb';
 
-function myFunction( param1:string, param2: string){
+import HelloWorld from '@/components/HelloWorld.vue';
+import TheWelcome from '@/components/TheWelcome.vue';
+
+function myFunction( param1: string, param2: string ) {
 
 	param1 = 'reassignment allowed';
 
@@ -22,16 +21,16 @@ function myFunction( param1:string, param2: string){
 	}
 
 	const obj = { a: 1, b: 2 };
-	for ( const [key, value] of Object.entries(obj) ) {
+	for ( const [ key, value ] of Object.entries( obj ) ) {
 		console.log( key, value );
 	}
+
 }
 
 myFunction( '1', '1' );
 
-const arrowFunc = (arg1: number, arg2: number ): number => {
-	return arg1 +arg2;
-
+const arrowFunc = ( arg1: number, arg2: number ): number => {
+	return arg1 + arg2;
 };
 
 const arrowFunc1Param = ( arg: string ) => {
@@ -42,7 +41,7 @@ const arrowFunc1Param = ( arg: string ) => {
 const obj = {
 	a: 1,
 	b: 2,
-	c: 3
+	c: 3,
 };
 
 const obj2 = {
@@ -55,51 +54,55 @@ const obj2 = {
 };
 
 const shorthand = 'test';
-const obj3 = { shorthand};
+const obj3 = { shorthand };
 
 </script>
 
-
 <template>
+	<header>
+		<img
+			alt="Vue logo"
+			class="logo"
+			src="./assets/logo.svg"
+			width="125"
+			height="125"
+		/>
 
-<header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+		<div class="wrapper">
+			<HelloWorld msg="You did it!" />
+		</div>
+	</header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<main>
+		<TheWelcome />
+	</main>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
+	line-height: 1.5;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+	display: block;
+	margin: 0 auto 2rem;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+	header {
+		display: flex;
+		place-items: center;
+		padding-right: calc(var(--section-gap) / 2);
+	}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+	.logo {
+		margin: 0 2rem 0 0;
+	}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+	header .wrapper {
+		display: flex;
+		place-items: flex-start;
+		flex-wrap: wrap;
+	}
 }
 </style>
